55dc9ea808cdeb659b72906529e62c884b5b8cf2
Christian <christianvillarrealt@gmail.com>
2024-05-13 18:09:19 -0600

    Changes to ADC NOW WORKS

--- Src/adc.c
+++ Src/adc.c
@@ -4,16 +4,16 @@
 
 void USER_ADC1_Init( void ) {
     // Configure ADC1
-    ADC1->CR1 |= ADC1_CR1_DUALMOD; // Select independent operation mode
-    ADC1->CR2 |= ADC1_CR2_ALIGN; // Right alignment
+    ADC1->CR1 &= ~(ADC1_CR1_DUALMOD); // Select independent operation mode
+    ADC1->CR2 &= ~(ADC1_CR2_ALIGN); // Right alignment
     ADC1->CR2 |= ADC1_CR2_CONT; // Continuous conversion mode
 
-    // Configure sample time for ADC channel 0
+    // Configure sample time for ADC channel 0 | 1.5 cycles channel sample time
     ADC1->SMPR2 &= ~(ADC1_SMPR2_SMP0);
 
     // Configure sequence and/or number of conversions for ADC regular channels
-    ADC1->SQR1 |= ADC1_SQR1_L; // 1 conversion for regular channels
-    ADC1->SQR3 |= ADC1_SQR3_SQ1; // Channel for the first ADC conversion | PA0
+    ADC1->SQR1 &= ~(ADC1_SQR1_L); // 1 conversion for regular channels
+    ADC1->SQR3 &= ~(ADC1_SQR3_SQ1); // Channel for the first ADC conversion | PA0
     // Since we will read from PA0, we don't need to include a POT_ADC_PIN in main.c
 
     // Enable ADC module
@@ -21,7 +21,7 @@ void USER_ADC1_Init( void ) {
 
     // Calibrate ADC module
     ADC1->CR2 |= ADC1_CR2_CAL;
-    while ( ADC1->CR2 & ADC1_CR2_CAL ); // Wait for calibration to complete
+    while (ADC1->CR2 & ADC1_CR2_CAL); // Wait for calibration to complete
 }
 
 uint16_t USER_ADC1_Read( void ) {
@@ -29,8 +29,8 @@ uint16_t USER_ADC1_Read( void ) {
     ADC1->CR2 |= ADC1_CR2_ADON;
 
     // Wait for conversion to complete
-    while (!(ADC1->SR & ( 0x1UL << 1U ))); // Wait until EOC (End of Conversion Flag) is set
+    while (!(ADC1->SR & ADC1_SR_EOC)); // Wait until EOC (End of Conversion Flag) is set
 
     // Read the converted value
-    return ADC1->DR;
+    return (uint16_t)ADC1->DR;
 }
--- Src/main.c
+++ Src/main.c
@@ -3,15 +3,17 @@
 #include "tim.h"
 #include "adc.h"
 #include "lcd.h"
+#include "adc.h"
+#include "lcd.h"
 #include "EngTrModel.h"
 
+/* Function prototypes */
 /* Function prototypes */
 void USER_RCC_Init(void);
 void USER_GPIO_Init(void);
 float map(float x, float in_min, float in_max, float out_min, float out_max);
 
-int main(void)
-{	
+int main(void) {
 	uint8_t col = 16;
 
   	USER_RCC_Init();
@@ -28,7 +30,23 @@ int main(void)
 	LCD_Init();
 
 	USER_ADC1_Init();
+	USER_TIM2_Init();
+	USER_TIM3_Init();
+	USER_TIM4_Init();
+	USER_TIM5_Init();
+	USER_TIM9_Init();
+	USER_TIM10_Init();
+	USER_TIM11_Init();
+
+	LCD_Init();
+
+	USER_ADC1_Init();
 	EngTrModel_initialize();
+
+	// ROW 1 - 'A' | Right movement (not used for now)
+	// ROW 2 - 'B' | Brake
+	// ROW 3 - 'C' | Left movement (not used for now)
+
 
 	// ROW 1 - 'A' | Right movement (not used for now)
 	// ROW 2 - 'B' | Brake
@@ -57,8 +75,31 @@ int main(void)
 			}
 		} else {
 			EngTrModel_U.Throttle = acceleration;
+
+		GPIOA->ODR ^= ( 0x1UL << 5U );
+		USER_TIM4_Delay();
+
+		// Reads the value from the potentiometer
+		uint16_t pot_value = USER_ADC1_Read();
+
+		// Scales the potentiometer value to the range of acceleration (0 to 100)
+		float acceleration = map(pot_value, 0, 4095, 0, 100);
+
+		// Key 'B' is pressed and executes brake
+		if ( !(ROW2_PIN) ) {
+			
+			USER_TIM2_Delay(); // 10ms delay for debounce
+
+			if ( !(ROW2_PIN) ) {
+				EngTrModel_U.Throttle = 0.0;
+				EngTrModel_U.BrakeTorque = 100.0;
+			}
+		} else {
+			EngTrModel_U.Throttle = acceleration;
 			EngTrModel_U.BrakeTorque = 0.0;
 		}
+
+		// calculate the model output values
 
 		// calculate the model output values
 		EngTrModel_step();
@@ -71,11 +112,53 @@ int main(void)
 		// printf("Vehicle Speed: %f\r\n", EngTrModel_Y.VehicleSpeed);
 		// printf("Engine Speed: %f\r\n", EngTrModel_Y.EngineSpeed);
 		// printf("Gear: %f\r\n", EngTrModel_Y.Gear);
+
+		// set the values in the msgs
+		msg[2] = (uint8_t) EngTrModel_Y.EngineSpeed;
+		msg[4] = (uint8_t) EngTrModel_Y.VehicleSpeed;
+		msg[6] = (uint8_t) EngTrModel_Y.Gear;
+
+		// printf("Vehicle Speed: %f\r\n", EngTrModel_Y.VehicleSpeed);
+		// printf("Engine Speed: %f\r\n", EngTrModel_Y.EngineSpeed);
+		// printf("Gear: %f\r\n", EngTrModel_Y.Gear);
+		}
 	}
 }
 
 void USER_RCC_Init(void){
 	
+	/* System Clock (SYSCLK) configuration for 64 MHz */
+	
+	FLASH->ACR	&=	~( 0x5UL << 0U ); // two wait states latency, if SYSCLK > 48 MHz
+	FLASH->ACR	|=	( 0x2UL << 0U ); // two wait states latency, if SYSCLK > 48 MHz
+	RCC->CFGR	&=	~( 0x1UL << 16U ) // PLL HSI clock /2 selected as PLL input clock
+				&	~( 0x7UL << 11U ) // APB2 prescaler /1
+				&	~( 0x3UL << 8U ) // APB1 prescaler /2 (APB1 must not exceed 36 MHz)
+				&	~( 0xFUL << 4U ); // AHB prescaler /1
+	RCC->CFGR	|=	 ( 0xFUL << 18U ) // PLL input clock x 16 (PLLMUL bits)
+				|	 ( 0x4UL << 8U ); // APB1 prescaler / 2
+	RCC->CR		|=	 ( 0x1UL << 24U ); // PLL2 on
+	while ( !(RCC->CR & ~( 0x1UL << 25U ))); // Wait until PLL is locked
+	RCC->CFGR	&=	~( 0x1UL << 0U ); // PLL used as system clock (SW bits)
+	RCC->CFGR	|=	 ( 0x2UL << 0U ); // PLL used as system clock (SW bits)
+	while ( 0x8UL != ( RCC->CFGR & 0xCUL )); // Wait until PLL is switched
+
+	RCC->CFGR	|=	 ( 0x3UL << 14U ); // PCLK2 divided by 8. PCLK2 is 64MHz and ADC clock is for 8 MHz
+	// Clock for the ADC peripheral is configured. Must not exceed 14 MHz
+
+	RCC->APB1ENR	|=	( 0x1UL << 0U ) // TIM2 clock enable
+					| ( 0x1UL << 1U ) 	// TIM3 clock enable
+					| ( 0x1UL << 2U ) 	// TIM4 clock enable
+					| ( 0x1UL << 3U );	// TIM5 clock enable
+
+	RCC->APB2ENR 	|= ( 0x1UL << 2U ) // IO Port A clock enable
+					| ( 0x1UL << 14U ) // USART 1 clock enable
+					| ( 0x1UL << 3U ) // IO Port B clock enable
+					| ( 0x1UL << 4U ) // IO Port C clock enable
+					| ( 0x1UL << 9U ) // Configured ADC1 clock is enabled
+					| ( 0x1UL << 19U ) // TIM9 clock enable
+					| ( 0x1UL << 20U ) // TIM10 clock enable
+					| ( 0x1UL << 21U ); // TIM11 clock enable
 	/* System Clock (SYSCLK) configuration for 64 MHz */
 	
 	FLASH->ACR	&=	~( 0x5UL << 0U ); // two wait states latency, if SYSCLK > 48 MHz
